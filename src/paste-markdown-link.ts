2bfdd84c834afbdef0cf11d1a688e03eeae1ed7e
John Bohannon <imjohnbo@github.com>
2022-01-24 21:01:17 +0000

    wip: paste multiple markdown links

--- src/paste-markdown-link.ts
+++ src/paste-markdown-link.ts
@@ -10,30 +10,70 @@ export function uninstall(el: HTMLElement): void {
 
 function onPaste(event: ClipboardEvent) {
   const transfer = event.clipboardData
-  if (!transfer || !hasPlainText(transfer)) return
+  if (!transfer || !hasPlainText(transfer) || !hasHTMLText(transfer)) return
 
   const field = event.currentTarget
   if (!(field instanceof HTMLTextAreaElement)) return
 
   const text = transfer.getData('text/plain')
   if (!text) return
-  if (!isURL(text)) return
-  if (isWithinLink(field)) return
 
-  const selectedText = field.value.substring(field.selectionStart, field.selectionEnd)
-  if (!selectedText.length) return
-  // Prevent linkification when replacing an URL
-  // Trim whitespace in case whitespace is selected by mistake or by intention
-  if (isURL(selectedText.trim())) return
+  if (isWithinSelection(field)) {
+    if (!isURL(text)) return
+    if (isWithinLink(field)) return
 
-  event.stopPropagation()
-  event.preventDefault()
+    const selectedText = field.value.substring(field.selectionStart, field.selectionEnd)
+    if (!selectedText.length) return
+    // Prevent linkification when replacing an URL
+    // Trim whitespace in case whitespace is selected by mistake or by intention
+    if (isURL(selectedText.trim())) return
 
-  insertText(field, linkify(selectedText, text))
+    event.stopPropagation()
+    event.preventDefault()
+
+    insertText(field, linkifyOld(selectedText, text))
+  } else {
+    const html = transfer.getData('text/html')
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html')
+
+    const a = doc.getElementsByTagName('a')
+    const markdown = transform(a, text, linkify)
+
+    if (markdown === text) return
+    
+    event.stopPropagation()
+    event.preventDefault()
+
+    insertText(field, markdown)
+  }
+}
+
+function transform(elements: HTMLCollectionOf<HTMLElement>, text: string, transformer: any, ...args: any): string {
+  let markdown = ''
+  Array.from(elements).forEach((element: HTMLElement) => {
+    const {part, index} = trimAfter(text, element.innerText)
+    markdown += part.replace(element.innerText, transformer(element, args))
+    text = text.slice(index)
+  })
+  markdown += text
+  return markdown
+}
+
+function trimAfter(text: string, search: string): {part: string, index: number} {
+  const index = text.indexOf(search) + search.length
+  return { 
+    part: text.substring(0, index),
+    index
+  }
 }
 
 function hasPlainText(transfer: DataTransfer): boolean {
   return Array.from(transfer.types).includes('text/plain')
+}
+
+function hasHTMLText(transfer: DataTransfer): boolean {
+  return Array.from(transfer.types).includes('text/html')
 }
 
 function isWithinLink(textarea: HTMLTextAreaElement): boolean {
@@ -47,8 +87,16 @@ function isWithinLink(textarea: HTMLTextAreaElement): boolean {
   }
 }
 
-function linkify(selectedText: string, text: string): string {
+function isWithinSelection(textarea: HTMLTextAreaElement): boolean {
+  return textarea.selectionStart !== textarea.selectionEnd
+}
+
+function linkifyOld(selectedText: string, text: string): string {
   return `[${selectedText}](${text})`
+}
+
+function linkify(element: HTMLAnchorElement): string {
+  return `[${element.innerText}](${element.href})`
 }
 
 function isURL(url: string): boolean {
